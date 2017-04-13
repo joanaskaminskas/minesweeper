f19c9a0886d994a90f77ec207b420a1c660cdb15
joanaskaminskas <joanas.kaminskas@gmail.com>
2017-04-13 14:58:13 +0300

    Add files via upload

---
+++ Minesweeper/src/com/zetcode/GameTimer.java
@@ -0,0 +1,44 @@
+package com.zetcode;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+public class GameTimer implements ActionListener {
+
+	private int seconds = 0;
+
+	private Timer clock = new Timer(1000, this);
+
+	JLabel statusbar;
+
+	public GameTimer(JLabel statusbar) {
+		this.statusbar = statusbar;
+	}
+
+	public void start() {
+		clock.start();
+	}
+
+	public void stop() {
+		clock.stop();
+	}
+
+	public void reset() {
+		seconds = 0;
+		statusbar.setText(seconds + " s");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		seconds++;
+		statusbar.setText(seconds + " s");
+		System.out.println(seconds);
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+
+}
---
+++ Minesweeper/src/com/zetcode/StatusBar.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import javax.swing.JFrame;
+
+public class StatusBar extends JFrame {
+	
+	private GameTimer timer;
+	
+	public GameTimer getTimer() {
+		return timer;
+	}
+
+}
